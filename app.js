3fd5a95247af3184d2e49dd31021d9ca24963dee
Ramsha07 <ramshaaijaz99@gmail.com>
2020-06-15 23:56:09 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,222 @@
+// -----------------Chapter#1 and task 1----------
+// alert("Hello!Welcome to My portfolio");
+
+
+
+// -----------------Chapter#1 and task 2----------
+// alert("Error! Please enter a valid password.");
+
+
+
+// -----------------Chapter#1 and task 3----------
+// alert("Welcome to JS Land... \nHappy Coding!\n");
+
+
+
+// -----------------Chapter#1 and task 3----------
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!\n Prevent this page from creating addit ional dialogs.");
+
+
+
+// -----------------Chapter#2 and task 1----------
+// var username;
+
+
+// -----------------Chapter#2 and task 2----------
+
+// var myName = "Ramsha Aijaz";
+
+// -----------------Chapter#2 and task 3--------
+// var message = "Hello World";
+// alert(message);
+
+// -----------------Chapter#2 and task 4--------
+// var name = "Jhone Doe";
+// var age = "15 years old";
+// var education = "Certified Mobile Application Development";
+// alert(name);
+// alert(age);
+// alert(education);
+
+// -----------------Chapter#2 and task 4--------
+// var fastFood = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+
+// alert(fastFood); 
+
+
+
+// -----------------Chapter#2 and task 6--------
+// var a = "My email address is ";
+// var email = "Ramshaaijaz99@gmail.com";
+// var b = a + email;
+// alert(b);
+
+
+
+// -----------------Chapter#2 and task 7--------
+// var a = "I am trying to learn from the Book ";
+// var book = "A smarter way to learn JavaScript";
+// var b = a + book;
+// alert(b);
+
+
+// -----------------Chapter#2 and task 8--------
+// var browser = "Yah! I can write HTML content through JavaScript";
+// document.write(browser);
+
+
+// -----------------Chapter#2 and task 9--------
+// var end = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬";
+// alert(end);
+// document.write(end);
+
+// -----------------Chapter#3 and task 1--------
+// var age = 20 ;
+// var age1 = "I am ";
+// var age2 = " years old";
+// var c = age1 + age + age2;
+// alert(c);
+
+
+// -----------------Chapter#3 and task 2--------
+// var age = 14 ;
+// var age1 = "You have visited this site ";
+// var age2 = " times";
+// var c = age1 + age + age2;
+// alert(c);
+
+// -----------------Chapter#3 and task 3--------
+// var birthYear = 1999;
+// var a = "My birth year is ";
+// var c =" Data type of my declared variable is number";
+// var b = a + birthYear ;
+
+// document.write(b);
+
+// document.write(" <br>Data type of my declared variable is number");
+// -----------------Chapter#3 and task 4--------
+// var a =  "<b>John Doe</b>";
+// var b ="<b> 5 </b>" ;
+// var c = "<b>T-shirt</b>(s)";
+// document.write(a);
+// document.write(" ordered");
+// document.write(b);
+// document.write(c);
+// document.write(" on XYZ Clothing store");
+
+// -----------------Chapter#4 and task 1--------
+// var name$dollar_myName;
+
+// -----------------Chapter#4 and task 2--------
+// ------Legal variable
+// var name;
+// var myName;
+// var $name;
+// var _name;
+// var name1;
+
+// ----Ilegal variable
+// var 1name;
+// var name-;
+// var name?;
+// var name#;
+// var 222name%;
+
+
+// -----------------Chapter#4 and task 3--------
+// var heading = "<b>Rules for naming JS variables</b>";
+// document.write(heading);
+// document.write("</br>Variable names can only contain numbers, $, letters and _. For example $my_1stVariable");
+// document.write("<br>Variables must begin with a letter, $ or _. For example $name, _name or name");
+// document.write("<br>Variable names are case sensitive");
+// document.write("<br>Variable names should not be JS keyword");
+
+
+
+// -----------------Chapter#5 and task 1--------
+// var firstNumber = 5;
+// var secondNumber = 3;
+// var sum ="Sum of 3 and 5 is " + (firstNumber + secondNumber);
+// document.write(sum);
+
+
+// -----------------Chapter#5 and task 2--------
+// SUBSTRACTION
+// var firstNumber = 5;
+// var secondNumber = 3;
+// var sum ="Substraction of 3 and 5 is " + (firstNumber - secondNumber);
+// document.write(sum);
+
+// MULTIPLICATION
+// var firstNumber = 5;
+// var secondNumber = 3;
+// var sum ="Multiplication of 3 and 5 is " + (firstNumber * secondNumber);
+// document.write(sum);
+
+// DIVISION
+// var firstNumber = 5;
+// var secondNumber = 3;
+// var sum ="Division of 3 and 5 is " + (firstNumber / secondNumber);
+// document.write(sum);
+// MODULUS
+// var firstNumber = 5;
+// var secondNumber = 3;
+// var sum ="Modulus of 3 and 5 is " + (firstNumber % secondNumber);
+// document.write(sum);
+
+
+// -----------------Chapter#5 and task 2--------
+// var newVariable;
+// document.write("Value after variable declaration is "+(newVariable));
+
+// newVariable = 5;
+// document.write("<br>Initial value : " + (newVariable));
+
+// newVariable = ++newVariable;
+// document.write("<br>Value after increment is: " + (newVariable));
+
+// newVariable = newVariable+7;
+// document.write("<br>Value after addition is: " + (newVariable));
+
+
+
+// newVariable = --newVariable;
+// document.write("<br>Value after decrement is: " + (newVariable));
+
+
+// newVariable = newVariable%3;
+// document.write("<br>The remainder is: " + (newVariable));
+
+
+var oneTicket = 600;
+var totalPerson = 5;
+var sum ="Total cost to buy 5 tickets to a movie is " + (oneTicket * totalPerson) + "PKR";
+document.write(sum);
+
+
+var table = 4;
+document.write()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
